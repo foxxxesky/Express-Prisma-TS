3b28fc4c612c5dd78febb3a49f14884d54317ce8
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-08-27 05:12:02 +0700

    update index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 
+import admin from './admin'
+import user from './user'
 import authentication from './authentication'
 
 const router = express.Router()
 
 export default(): express.Router => {
     authentication(router)
-    
+    admin(router)
+    user(router)
+
     return router
 }
